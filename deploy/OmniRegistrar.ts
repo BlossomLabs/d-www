1b0715186527340f2b0b6789eb6853468c023638
Sem <931684+sembrestels@users.noreply.github.com>
2024-11-17 00:22:58 +0700

    Create deploy and layerzero config

---
+++ deploy/OmniRegistrar.ts
@@ -0,0 +1,56 @@
+import assert from 'assert'
+
+import { type DeployFunction } from 'hardhat-deploy/types'
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+
+const contractName = 'OmniRegistrar'
+
+const deploy: DeployFunction = async (hre) => {
+    const { getNamedAccounts, deployments } = hre
+
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    assert(deployer, 'Missing named deployer account')
+
+    console.log(`Network: ${hre.network.name}`)
+    console.log(`Deployer: ${deployer}`)
+
+    // This is an external deployment pulled in from @layerzerolabs/lz-evm-sdk-v2
+    //
+    // @layerzerolabs/toolbox-hardhat takes care of plugging in the external deployments
+    // from @layerzerolabs packages based on the configuration in your hardhat config
+    //
+    // For this to work correctly, your network config must define an eid property
+    // set to `EndpointId` as defined in @layerzerolabs/lz-definitions
+    //
+    // For example:
+    //
+    // networks: {
+    //   fuji: {
+    //     ...
+    //     eid: EndpointId.AVALANCHE_V2_TESTNET
+    //   }
+    // }
+    const omninameDeployment = await hre.deployments.get('OmniName')
+
+    const args = [
+        omninameDeployment.address,
+        EndpointId.SCROLL_V2_MAINNET
+    ]
+
+    const { address } = await deploy(contractName, {
+        from: deployer,
+        args,
+        log: true,
+        skipIfAlreadyDeployed: false,
+    })
+
+    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${address}`)
+    console.log(`To verify contract run: npx hardhat verify --network ${hre.network.name} ${address} ${args.join(' ')}`)
+}
+
+deploy.tags = [contractName]
+deploy.dependencies = ['OmniName']
+
+export default deploy
--- layerzero.config.ts
+++ layerzero.config.ts
@@ -3,20 +3,35 @@ import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
 
 import type { OAppEdgeConfig, OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
 
-const celoContract: OmniPointHardhat = {
+const celoOmniRegistrar: OmniPointHardhat = {
+    eid: EndpointId.CELO_V2_MAINNET,
+    contractName: 'OmniRegistrar',
+}
+
+const celoOmniName: OmniPointHardhat = {
     eid: EndpointId.CELO_V2_MAINNET,
     contractName: 'OmniName',
 }
 
-const scrollContract: OmniPointHardhat = {
+const scrollL2Registrar: OmniPointHardhat = {
+    eid: EndpointId.SCROLL_V2_MAINNET,
+    contractName: 'L2Registrar',
+}
+
+const scrollOmniName: OmniPointHardhat = {
     eid: EndpointId.SCROLL_V2_MAINNET,
     contractName: 'OmniName',
 }
 
-const morphContract: OmniPointHardhat = {
-    eid: EndpointId.MORPH_V2_MAINNET,
-    contractName: 'OmniName',
-}
+// const morphOmniRegistrar: OmniPointHardhat = {
+//     eid: EndpointId.MORPH_V2_MAINNET,
+//     contractName: 'OmniRegistrar',
+// }
+
+// const morphOmniName: OmniPointHardhat = {
+//     eid: EndpointId.MORPH_V2_MAINNET,
+//     contractName: 'OmniName',
+// }
 
 const DEFAULT_EDGE_CONFIG: OAppEdgeConfig = {
     enforcedOptions: [
@@ -39,46 +54,77 @@ const DEFAULT_EDGE_CONFIG: OAppEdgeConfig = {
 const config: OAppOmniGraphHardhat = {
     contracts: [
         {
-            contract: scrollContract,
+            contract: scrollOmniName,
         },
         {
-            contract: celoContract,
+            contract: scrollL2Registrar,
         },
         {
-            contract: morphContract,
+            contract: celoOmniName,
         },
+        {
+            contract: celoOmniRegistrar,
+        },
+        // {
+        //     contract: morphOmniName,
+        // },
+        // {
+        //     contract: morphOmniRegistrar,
+        // },
     ],
     connections: [
+        // OmniName are configured to be the same on all chains (Star topology)
         {
-            from: scrollContract,
-            to: celoContract,
+            from: scrollOmniName,
+            to: celoOmniName,
             config: DEFAULT_EDGE_CONFIG,
         },
+        // {
+        //     from: scrollOmniName,
+        //     to: morphOmniName,
+        //     config: DEFAULT_EDGE_CONFIG,
+        // },
         {
-            from: scrollContract,
-            to: morphContract,
+            from: celoOmniName,
+            to: scrollOmniName,
             config: DEFAULT_EDGE_CONFIG,
         },
+        // {
+        //     from: celoOmniName,
+        //     to: morphOmniName,
+        //     config: DEFAULT_EDGE_CONFIG,
+        // },
+        // {
+        //     from: morphOmniName,
+        //     to: celoOmniName,
+        //     config: DEFAULT_EDGE_CONFIG,
+        // },
+        // {
+        //     from: morphOmniName,
+        //     to: scrollOmniName,
+        //     config: DEFAULT_EDGE_CONFIG,
+        // },
+        // L2Registrar - OmniRegistrar is configured with a Tree topology
         {
-            from: celoContract,
-            to: scrollContract,
-            config: DEFAULT_EDGE_CONFIG,
+            from: scrollL2Registrar,
+            to: celoOmniRegistrar,
+            // config: DEFAULT_EDGE_CONFIG,
         },
         {
-            from: celoContract,
-            to: morphContract,
-            config: DEFAULT_EDGE_CONFIG,
+            from: celoOmniRegistrar,
+            to: scrollL2Registrar,
+            // config: DEFAULT_EDGE_CONFIG,
         },
-        {
-            from: morphContract,
-            to: celoContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
-        {
-            from: morphContract,
-            to: scrollContract,
-            config: DEFAULT_EDGE_CONFIG,
-        },
+        // {
+        //     from: scrollL2Registrar,
+        //     to: morphOmniRegistrar,
+        //     // config: DEFAULT_EDGE_CONFIG,
+        // },
+        // {
+        //     from: morphOmniRegistrar,
+        //     to: scrollL2Registrar,
+        //     // config: DEFAULT_EDGE_CONFIG,
+        // },
     ],
 }
 
